cc0eddc909aa462815c4b4c1b10d97112e34d430
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2022-02-25 22:59:34 +0100

    Added better input handling function

--- src/player.gd
+++ src/player.gd
@@ -1,6 +1,7 @@
-extends KinematicBody2D
+extends StaticBody2D
 
-
+# sprite is 32x32, scaled to 0.5 on both axes
+var dimensions := Vector2(16, 16)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,7 +12,14 @@ func _ready():
 	pass # Replace with function body.
 
 
+func get_input_direction() -> Vector2:
+	return Vector2(
+		int(Input.is_action_just_pressed("ui_right")) - int(Input.is_action_just_pressed("ui_left")),
+		int(Input.is_action_just_pressed("ui_up")) - int(Input.is_action_just_pressed("ui_down"))
+	)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_right"):
-		move_and_slide_with_snap(Vector2(32/delta,0),Vector2(0,-1))
+		self.position.x
