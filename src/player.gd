35b1e5825b708dbee2def8b8ec8abd3083c53d92
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2022-02-27 00:49:42 +0100

    Movement is working. Now work on collisions and pushing

--- src/player.gd
+++ src/player.gd
@@ -1,7 +1,7 @@
 extends StaticBody2D
 
 # sprite is 32x32, scaled to 0.5 on both axes
-var dimensions := Vector2(16, 16)
+var dimensions: Vector2 = Vector2(64, 64) * self.scale
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -15,11 +15,11 @@ func _ready():
 func get_input_direction() -> Vector2:
 	return Vector2(
 		int(Input.is_action_just_pressed("ui_right")) - int(Input.is_action_just_pressed("ui_left")),
-		int(Input.is_action_just_pressed("ui_up")) - int(Input.is_action_just_pressed("ui_down"))
+		int(Input.is_action_just_pressed("ui_down")) - int(Input.is_action_just_pressed("ui_up"))
 	)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_right"):
-		self.position.x
+func _physics_process(_delta) -> void:
+	var direction_vector: Vector2 = get_input_direction()
+	self.position += (direction_vector * dimensions)
