e6ce32d6afd77660b4cdbbb1d3b1ef5bf601eb01
Carlos A. Parra F <carlos.a.parra.f@gmail.com>
2022-07-05 17:48:53 +0200

    Started working on animations, but player can still change movement direction mid-animation

--- Scenes/Share/grid_piece.gd
+++ Scenes/Share/grid_piece.gd
@@ -2,9 +2,23 @@ class_name GridPiece
 extends StaticBody2D
 
 onready var grid: Grid = get_parent()
-
+onready var tween: Tween = $Tween
+export var time_animation: float = 0.25
 
 # Basic move function:
 # TODO: expand once animations are implemented
 func move_to(new_pos: Vector2) -> void:
-	self.position = new_pos
+	set_process(false)
+	var _unused = tween.interpolate_property(
+		self,
+		"position",
+		self.position,
+		new_pos,
+		time_animation,
+		Tween.TRANS_SINE,
+		Tween.EASE_OUT
+	)
+	_unused = tween.start()
+	yield(tween, "tween_completed")
+	set_process(true)
+	# self.position = new_pos
